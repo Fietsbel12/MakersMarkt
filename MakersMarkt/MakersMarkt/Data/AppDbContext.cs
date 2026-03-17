d46dcec740c23a37f127dfbd91f849b800e410c2
Fietsbel12 <Lotusk20@outlook.com>
2026-03-17 15:18:39 +0100

    Add App.config, example and EF Core DbContext

---
+++ MakersMarkt/MakersMarkt/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakersMarkt.Data
+{
+    class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                ConfigurationManager.ConnectionStrings["MakersMarkt"].ConnectionString,
+                ServerVersion.Parse("8.0.30")
+                );
+        }
+    }
+}
