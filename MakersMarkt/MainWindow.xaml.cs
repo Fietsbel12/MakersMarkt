c6cc347e56733277d34fa61210649f61a07666dc
Fietsbel12 <Lotusk20@outlook.com>
2026-03-20 14:27:25 +0100

    Recreate database on MainWindow startup
    Add AppDbContext usage in MainWindow and call Database.EnsureDeleted() and EnsureCreated() in the constructor. This forces the application's database to be recreated on startup (useful for development/testing), and includes the required using directive and minor using reordering.

--- MakersMarkt/MainWindow.xaml.cs
+++ MakersMarkt/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using MakersMarkt.Data;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,6 +6,11 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -26,6 +27,11 @@ namespace MakersMarkt
         public MainWindow()
         {
             InitializeComponent();
+
+            using var db = new AppDbContext();
+
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
         }
     }
 }
